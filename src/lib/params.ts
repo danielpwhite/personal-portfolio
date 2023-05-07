51bff6550d25852dec8a3c3419c4d75ceaf5fc9d
Daniel White <daniel.p.white@hotmail.com>
2023-05-07 14:22:10 -0500

    fixed project logo

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -41,7 +41,7 @@ export const PersonalProjects: PersonalProjectsParams = {
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
 			links: [{ to: 'https://github.com/danielpwhite/Web-App-Deployed', label: 'GitHub' }],
-			logo: Assets.Unknown,
+			logo: Assets.Python,
 			name: 'Web-App-Deployed',
 			period: {
 				from: new Date()
